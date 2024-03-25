c58b0e04839366af1b50bb835c55afd393e301c4
crazywizard <david.ngugi@engie.com>
2024-03-25 21:39:27 +0300

    Create UWSGI pid dir

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Set working directory
-cd /var/www/html/django_project/sre_challenge
-
-# set up virtualenv
-/usr/bin/python3 -m venv venv
-source venv/bin/activate
-
-# Install python requirements
-pip install -r requirements.txt
+# Create uwsgi pid dir
+mkdir -p /var/run/uwsgi
