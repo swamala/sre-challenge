3128d2a938cfa4580dac53359a7d509fd3710db3
crazywizard <david.ngugi@engie.com>
2024-03-26 10:19:21 +0300

    Change venv dir

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,8 +4,8 @@
 cd /var/www/html/django_project/sre_challenge
 
 # set up virtualenv
-/usr/bin/python3 -m venv venv
-source venv/bin/activate
+/usr/bin/python3 -m venv .venv
+source .venv/bin/activate
 
 # Install python requirements
 pip install -r requirements.txt
