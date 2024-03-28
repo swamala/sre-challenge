4d884b15f66a7c9bf63889846ce71823edc7c1e9
crazywizard <david.ngugi@engie.com>
2024-03-28 14:16:19 +0300

    CD into CWD for application start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Start webserver
+cd /var/www/html/django_project/sre_challenge
 sudo /var/www/html/django_project/sre_challenge/.venv/bin/uwsgi --socket /var/run/uwsgi/app/socket --module sre_challenge.wsgi:application --daemonize /var/log/uwsgi/sre-challenge.log
+
 # Start reverse proxy
 sudo systemctl start nginx.service
